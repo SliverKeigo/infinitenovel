f1c71fd1c1476f422810d32eea028a1f7454d69b
Keigo <sliverkeigo343@gmail.com>
2025-06-15 23:28:30 +0800

    feat: Setup environment and tooling for DB migration
     - Install 'pg' and 'dotenv' packages.
    
     - Configure Next.js to handle environment variables and WebAssembly.
    
     - Add ESLint configuration.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack(config, { isServer, dev }) {
+    config.experiments = {
+      ...config.experiments,
+      asyncWebAssembly: true,
+    };
+    config.output.webassemblyModuleFilename =
+      (isServer ? "../" : "") + "static/wasm/[modulehash].wasm";
+    return config;
+  },
+  experimental: {},
 };
 
 export default nextConfig;
